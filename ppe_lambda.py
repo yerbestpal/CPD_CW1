50ec97374e2cb4cee06b490d564ffde7fe191ee9
Ross McLean <ri.mcl@icloud.com>
2022-08-09 20:09:52 +0100

    Update ppe_lambda.py - Clean code - Comment code

--- ppe_lambda.py
+++ ppe_lambda.py
@@ -1,66 +1,70 @@
 import json
-from urllib import response
 import boto3
+from decimal import Decimal
 
 def lambda_handler(event, context):
   s3_client = boto3.client('s3')
   client = boto3.client('rekognition')
 
+  # Loop Records
   for msg in event['Records']:
-    msg_payload = json.loads(msg['body'])
+    print(msg)
 
-    if 'Records' in msg_payload:
-      bucket = msg_payload['Records'][0]['s3']['bucket']['name']
-      image = msg_payload['Records'][0]['s3']['object']['key'].replace('+', ' ')
-      response = client.detect_protective_equipment(
-        Image={
-          'S3Object': {
-            'Bucket': bucket,
-            'Name': image
+    bucket = 'bucket-s2030507'
+    image = str(msg['body'])
+
+    # Detect faces and hands in image
+    response = client.detect_protective_equipment(
+      Image={
+        'S3Object': {
+          'Bucket': bucket,
+          'Name': image
+        }
+      },
+      SummarizationAttributes={
+        'MinConfidence': 50,
+        'RequiredEquipmentTypes': [
+          'FACE_COVER',
+          'HAND_COVER'
+        ]
+      }
+    )
+
+    results = []
+
+    # Loop individuals in results
+    for person in response['Persons']:
+      body_parts = person['BodyParts']
+      result = {
+        'Image_Name': image,
+        'Details': []
+      }
+
+      # Loop body parts to gain details
+      for equipment_detections in body_parts:
+        name = equipment_detections['Name']
+        ppe = equipment_detections['EquipmentDetections']
+        for ppe_type in ppe:
+          types = ppe_type['Type']
+          confidence = ppe_type['Confidence']
+          covers_body = ppe_type['CoversBodyPart']['Value']
+
+          person_details = {
+            'Body Part': name,
+            'Confidence': confidence,
+            'Cover Type': types,
+            'Covers Body Part': covers_body
           }
-        },
-        SummarizationAttributes={
-          'MinConfidence': 80,
-          'RequiredEquipmentTypes': [
-            'FACE_COVER',
-            'HAND_COVER'
-          ]
-        }
-      )
-
-      results = []
-
-      for person in response['Persons']:
-        body_parts = person['BodyParts']
-        result = {
-          'Image_Name': image,
-          'Details': []
-        }
-
-        for equipment_detections in body_parts:
-          name = equipment_detections['Name']
-          ppe = equipment_detections['EquipmentDetections']
-          for ppe_type in ppe:
-            types = ppe_type['Type']
-            confidence = ppe_type['Confidence']
-            covers_body = ppe_type['CoversBodyPart']['Value']
-
-            person_details = {
-              'Body Part': name,
-              'Confidence': confidence,
-              'Cover Type': types,
-              'Covers Body Part': covers_body
-            }
 
           result['Details'].append(person_details)
-        
-        if len(result['Details']) > 0:
           results.append(result)
 
-        table = boto3.resource('dynamodb').Table('S2030507_Image_Data')
-        table.put_item(
-          Item={
+          # Build item from results
+          item = json.loads(json.dumps({
             'Image_Name': image,
             'Labels': result
-          }
-        )
+          }), parse_float=Decimal)
+
+          # Upload item to S3
+          table = boto3.resource('dynamodb').Table('S2030507_Image_Data')
+          table.put_item(Item=item)
