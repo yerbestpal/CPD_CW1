237163d2aff1aed2c72ab1954355766e0a5a8452
Ross McLean <ri.mcl@icloud.com>
2022-08-09 20:45:22 +0100

    Update sms_lambda.py

--- sms_lambda.py
+++ sms_lambda.py
@@ -2,10 +2,10 @@ import json
 import boto3
 
 # Send SMS
-def send_sms():
+def send_sms(event_body):
   sns = boto3.client('sns')
   phone_number = '+447722509271'
-  message_content = 'Message successful!'
+  message_content = str(event_body) + 'does not contain any PPE!'
   message_attributes = {
     'AWS.SNS.SMS.SMSType': {
         'DataType': 'String',
@@ -19,10 +19,17 @@ def send_sms():
   )
   
 def lambda_handler(event, context):
-  # Call function
-  print("It worked!")
-  send_sms(event, context)
+  s3_client = boto3.client('s3')
+  table = boto3.resource('dynamodb').Table('S2030507_Image_Data')
+  results = []
+  for item in table.scan()['Items']:
+    results.append(str(item['Image_Name']))
+
+  event_body = event['Records'][0]['body']
+  if event_body not in results:
+    send_sms(event_body)
+    print('SMS sent!' + '\n' + str(event_body) + 'does not contain any PPE!' + '\n')
   return {
       'statusCode': 200,
-      'body': json.dumps(str(event['Records'][0]))
+      'body': json.dumps(event)
   }
